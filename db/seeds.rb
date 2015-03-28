70a7347cef17fec11364a306c3dd8a097004de7b
Brian Stone <brianpatrickstone@gmail.com>
2015-03-28 00:13:05 -0600

    Complete Seed Data assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,24 @@
-require 'faker'
+#require 'faker'
 
 # Create Posts
-50.times do 
-  Post.create!(
-    title: Faker::Lorem.sentence,
-    body: Faker::Lorem.paragraph
-    )
-end
-posts = Post.all 
+# 50.times do 
+#   Post.create!(
+#     title: Faker::Lorem.sentence,
+#     body: Faker::Lorem.paragraph
+#     )
+# end
+# posts = Post.all 
 
+unique_post = Post.find_or_create_by(title: "It's up to me now...", body: "Turn on the bright lights...")
+unique_post
 # Create Comments
-100.times do 
-  Comment.create!(
-    post: posts.sample,
-    body: Faker::Lorem.paragraph
-    )
-end
+# 100.times do 
+#   Comment.create!(
+#     post: posts.sample,
+#     body: Faker::Lorem.paragraph
+#     )
+# end
+Comment.find_or_create_by(post: unique_post, body: "This is one of my favorite songs!")
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
