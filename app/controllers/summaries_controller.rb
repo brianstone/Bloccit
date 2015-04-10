dbb3f09911a35d914f06b7a69c93e9733b735dec
Brian Stone <brianpatrickstone@gmail.com>
2015-04-09 22:47:27 -0600

    Summary additions/changes

--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -1,7 +1,7 @@
 class SummariesController < ApplicationController
   def show
-    @post = Post.find(params[:id])
-    @summary = Summary.find(params[:post_id])
+    @post = Post.find(params[:post_id])
+    @summary = Summary.find(params[:id])
   end
 
   def new
@@ -14,7 +14,7 @@ class SummariesController < ApplicationController
     @summary = Summary.new(params.require(:summary).permit(:description))
     if @summary.save
       flash[:notice] = "Summary was saved"
-      redirect_to @summary
+      redirect_to [@post.topic, @post, @summary]
     else
       flash[:error] = "There was an error saving the summary. Please try again."
       render :new
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'summaries/show'
-
-  get 'summaries/new'
-
   devise_for :users
   resources :users, only: [:update]
 
