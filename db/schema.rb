cd0ee0d8dfafb2f3e3013152a78aeca7ca079f47
Brian Stone <brianpatrickstone@gmail.com>
2015-04-21 18:38:19 -0600

    migrated images to post

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20150421235438) do
-=======
-ActiveRecord::Schema.define(version: 20150412044823) do
->>>>>>> uploading-images-assignment
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -43,11 +39,8 @@ ActiveRecord::Schema.define(version: 20150412044823) do
     t.datetime "updated_at", null: false
     t.integer  "user_id"
     t.integer  "topic_id"
-<<<<<<< HEAD
     t.float    "rank"
-=======
     t.string   "image"
->>>>>>> uploading-images-assignment
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
